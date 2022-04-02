9306374259ee1f3ac910c308d72e7d523c550823
upskubra <yildirimk268@gmail.com>
2022-04-02 17:27:33 +0300

    set AppBarConfiguration

--- app/src/main/java/com/example/rick_and_morty/MainActivity.kt
+++ app/src/main/java/com/example/rick_and_morty/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.rick_and_morty
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.rick_and_morty.databinding.ActivityMainBinding
 
@@ -14,8 +17,24 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val navHostFragment =
+            // as for type casts
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
+
+
+
+        setupAppBarConfiguration(navController)
+    }
+
+    private fun setupAppBarConfiguration(navController: NavController) {
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.characterListFragment,
+                R.id.episodeListFragment,
+                R.id.locationListFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
