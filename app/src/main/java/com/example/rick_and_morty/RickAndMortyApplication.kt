513118f26e02b4f4db067134edb99486d8744d73
upskubra <yildirimk268@gmail.com>
2022-06-09 01:10:46 +0300

    create RickAndMortyApplication class

---
+++ app/src/main/java/com/example/rick_and_morty/RickAndMortyApplication.kt
@@ -0,0 +1,8 @@
+package com.example.rick_and_morty
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RickAndMortyApplication : Application() {
+}
